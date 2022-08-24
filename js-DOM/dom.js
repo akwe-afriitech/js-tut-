46b04d54ae8e00c2c41fbe5b808117ee07f3ffef
Earl Millen Toh Akwe <69284908+akwe-afriitech@users.noreply.github.com>
2022-08-24 03:47:09 +0100

    Add files via upload

---
+++ js-DOM/dom.js
@@ -0,0 +1,8 @@
+// document.getElementById("").style.color = "red";
+// let fruits = document.getElementsByClassName("fruits");
+
+let tags = document.getElementsByTagName("h2");
+tags[0].style.color = "blue";
+Document.querySelector(".h1");
+let all = document.querySelectorAll(".fruits");
+all[2].style.color = "red";
